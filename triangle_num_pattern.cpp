4f9f0c13fdcc07c63e110fad79dfee81391f0084
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-01-28 23:45:42 +0530

    Update triangle_num_pattern.cpp

--- triangle_num_pattern.cpp
+++ triangle_num_pattern.cpp
@@ -3,11 +3,9 @@ using namespace std;
 int main() {
     int n=4;
     for(int i=0; i<n; i++){ 
-        int num = 1;
         for(int j=0; j< i+1; j++){ 
             cout<< j+1;
         }
-        num++;
         cout << endl;
     }
     return 0;
