abf5945d65b3d2ad7947e450f35352d0eb54fb62
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-01-28 19:50:07 +0530

    Create square_pattern.cpp

---
+++ square_pattern.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+int main() {
+    int n=3;
+    int num = 1;
+    for(int i=0; i<n; i++){ //outer loop
+        for(int j=0; j<n; j++){ //inner loop
+            cout << num;
+            num++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
