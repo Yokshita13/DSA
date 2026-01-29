0477295d08462ca7bbade56b647345c76b9335c5
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-01-29 20:07:41 +0530

    Create pyramid_pattern.cpp

---
+++ pyramid_pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n=4;
+    for(int i=0; i<n; i++){
+        //spaces
+        for(int j=0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        //number1
+        for(int j=0; j<i+1; j++){
+            cout<<j+1;
+        }
+        //number2
+        for(int j=i; j>0; j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
