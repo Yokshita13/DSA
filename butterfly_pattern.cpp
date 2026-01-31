4597394ea62868147a6f5e7c319cc5b8f0cf4511
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-01-31 13:46:44 +0530

    Update butterfly_pattern.cpp

--- butterfly_pattern.cpp
+++ butterfly_pattern.cpp
@@ -10,10 +10,7 @@ int main() {
             cout<<"*";
         }
         //spaces
-        for(int j=0; j<n-i-1; j++){
-            cout<<" ";
-        }
-        for(int j=0; j<n-i-1; j++){
+        for(int j=0; j<2*(n-i-1); j++){
             cout<<" ";
         }
         //loop2
