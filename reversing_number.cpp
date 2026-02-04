76ecb7184ccd2b13f7bca52ea2780b58f0c6a6a6
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-02-04 18:09:23 +0530

    Create reversing_number.cpp

---
+++ reversing_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int revNum(int num){
+    int lastdigit; 
+    int revNum=0; 
+    while(num>0){
+        int lastdigit =  num % 10;
+        revNum = (revNum*10) + lastdigit; 
+        num = num/10; 
+    }
+    return revNum;
+} 
+int main() {
+    int num=125;
+    cout<< "Reversed Num : " << revNum(num) << endl; 
+    return 0; 
+}
