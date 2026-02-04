a04ee97001be109c21d5adcbe42118622663f9d2
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-02-04 17:29:17 +0530

    Create bitwise_operator.cpp

---
+++ bitwise_operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=6, b=10;
+    cout<< (a&b)<<endl;
+    cout<< (a|b)<<endl;
+    cout<< (a^b)<<endl;
+    cout<< (b<<2)<<endl;
+    cout<< (b>>1)<<endl;
+    return 0;
+}
