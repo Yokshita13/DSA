a05c2c125bb5bebf3a26d5736ac40a60e5508b7d
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-02-07 18:12:46 +0530

    Create reverseArray.cpp

---
+++ reverseArray.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {4,2,7,8,1,2,5};
+    int size = 7;
+    for(int i=size-1; i>=0; i--){
+        cout<< arr[i]<<" ";
+    }
+    return 0;
+}
