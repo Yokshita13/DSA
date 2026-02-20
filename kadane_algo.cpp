6b40fa8c00d96d64ba9a70aeeb880e63305d02cb
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-02-20 14:26:53 +0530

    Create kadane_algo.cpp

---
+++ kadane_algo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<climits>
+using namespace std;
+
+int main() {
+    int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+    int size = 9;
+    int currSum = 0;
+    int maxSum = INT_MIN;
+    for(int i=0; i<size; i++){
+        currSum += arr[i];
+        maxSum = max(currSum, maxSum);
+        if(currSum < 0){
+            currSum = 0;
+        }
+    }
+    cout<<maxSum;
+    return 0;
+}
