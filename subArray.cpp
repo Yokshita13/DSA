5581e27d4e5be5da5142ec250085e2fba147cf41
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-02-20 13:13:17 +0530

    Update subArray.cpp

--- subArray.cpp
+++ subArray.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
     int n=5;
     int arr[5] = {1,2,3,4,5};
-    for(int start=0; start<n; start++){
-        for(int end=start; end<n; end++){
-            for(int i=start; i<end; i++){
+    for(int start=0; start<n; start++){ //Start index
+        for(int end=start; end<n; end++){ //End index
+            for(int i=start; i<end; i++){ //Print elements between start and end
                 cout<<arr[i];
             }
             cout<<" ";
