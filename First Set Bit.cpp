c9cb45cec2fb3f824011147ccdbed83d5d933774
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-09-05 15:13:02 +0530

    Create First Set Bit.cpp

---
+++ First Set Bit.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int getFirstSetBit(int n) {
+        if(n == 0)
+            return 0;
+
+        int pos = 1;
+
+        while((n & 1) == 0) {
+            n >>= 1;
+            pos++;
+        }
+
+        return pos;
+    }
+};
