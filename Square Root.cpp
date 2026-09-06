c4b79e1bfe217efc4e7226e5175c324af4e77de8
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-09-06 18:29:01 +0530

    Refactor floorSqrt to use a simpler approach

--- Square Root.cpp
+++ Square Root.cpp
@@ -1,20 +1,10 @@
 class Solution {
   public:
     int floorSqrt(int n) {
-        int start = 1;
-        int end = n;
-        int res = 0;
-        
-        while(start<=end){
-            int mid = start + (end - start) /2;
-            if(mid*mid<=n){
-                res = mid;
-                start = mid + 1;
-            }
-            else{
-                end = mid - 1;
-            }
+        int i = 1;
+        while(i*i <= n){
+            i++;
         }
-        return res;
+        return i-1;
     }
 };
