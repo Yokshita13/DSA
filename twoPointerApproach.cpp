85b05d6bea956c635fe50c517f0a2bc28f19e36d
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-09-07 14:02:38 +0530

    Create twoPointerApproach.cpp

---
+++ twoPointerApproach.cpp
@@ -0,0 +1,10 @@
+class Solution{
+  public:
+      bool isPair(int arr[], int n, int x){
+        for(int i=0; i<n-1; i++)
+          for(int j=i+1; j<n; j++)
+            if(arr[i] + arr[j] == x)
+              return true;
+        return false;
+      }
+};
