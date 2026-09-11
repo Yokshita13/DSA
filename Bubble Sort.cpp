bec8c9017a649ac517c7cef658b369688ddfac90
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-09-11 15:02:05 +0530

    Implement bubble sort algorithm in Bubble Sort.cpp

---
+++ Bubble Sort.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        for(int i=0; i<arr.size() - 1; i++){
+            for(int j=0; j<arr.size()-i-1; j++){
+                if(arr[j]>arr[j+1])
+                    swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+};
